e380c9c11ad61874a666ec977b9700ba37357c6a
Vladislav <vsoilol1701@gmail.com>
2022-03-16 19:18:38 +0300

    Added ATM

--- homework3/homework3_1/src/main/java/Main.java
+++ homework3/homework3_1/src/main/java/Main.java
@@ -1,20 +1,10 @@
-import ATMActions.AddMoney;
-import ATMActions.ConvertMoneyByExchangeRate;
-import ATMActions.DisplayBalance;
-import ATMActions.WithdrawMoney;
 import Card.Card;
-import ConsoleMenu.Menu;
 
 public class Main {
     public static void main(String[] args) {
+        ATM atm = new ATM();
         Card card = new Card("Vlad");
 
-        Menu atm = new Menu("ATM (account holder: " + card.getHolderName() + ")");
-        atm.addMenuItem(new DisplayBalance(card));
-        atm.addMenuItem(new AddMoney(card));
-        atm.addMenuItem(new WithdrawMoney(card));
-        atm.addMenuItem(new ConvertMoneyByExchangeRate(card));
-
-        atm.display();
+        atm.run(card);
     }
 }
