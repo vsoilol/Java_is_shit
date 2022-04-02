f446b1fef37cae834a3543da6a0f9ee832b4bf9f
Vladislav <vsoilol1701@gmail.com>
2022-04-02 18:51:33 +0300

    Added validation for filesName.length

--- homework5/homework5_1/src/main/java/Validators/FileSystemBuilderValidator.java
+++ homework5/homework5_1/src/main/java/Validators/FileSystemBuilderValidator.java
@@ -9,5 +9,10 @@ public class FileSystemBuilderValidator {
         if (!Objects.equals(rootFolderName, filesName[0])) {
             throw new IllegalArgumentException(ErrorMessages.FOLDER_DOES_NOT_EXIST.formatted(filesName[0]));
         }
+
+        if(filesName.length == 1){
+            String errorMessage = ErrorMessages.ALREADY_EXIST.formatted(rootFolderName);
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
