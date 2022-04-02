a920431761521d2cf23308ff2f7a058c01fbb574
Vladislav <vsoilol1701@gmail.com>
2022-04-02 18:59:02 +0300

    Added SimpleMenuFacade

--- homework5/homework5_1/src/main/java/Main.java
+++ homework5/homework5_1/src/main/java/Main.java
@@ -1,23 +1,8 @@
-import Console.ConsoleProvider;
-import Console.FilePresenter;
-import Console.SimpleMenu;
-import FileSystem.FileSystemBuilder;
-import FileSystem.FileSystemTree;
-import Validators.FilePresenterValidator;
-import Validators.FileSystemBuilderValidator;
+import Console.SimpleMenuFacade;
 
 public class Main {
     public static void main(String[] args) {
-        FilePresenterValidator filePresenterValidator = new FilePresenterValidator();
-        FileSystemBuilderValidator fileSystemBuilderValidator = new FileSystemBuilderValidator();
-
-        ConsoleProvider consoleProvider = new ConsoleProvider();
-
-        FileSystemBuilder builder = new FileSystemBuilder(fileSystemBuilderValidator, "root");
-        FileSystemTree fileSystemTree = new FileSystemTree();
-        FilePresenter filePresenter = new FilePresenter(builder, fileSystemTree, filePresenterValidator, consoleProvider);
-
-        SimpleMenu simpleMenu = new SimpleMenu(filePresenter, consoleProvider);
-        simpleMenu.runMenu();
+        SimpleMenuFacade simpleMenuFacade = new SimpleMenuFacade();
+        simpleMenuFacade.runMenu();
     }
 }
