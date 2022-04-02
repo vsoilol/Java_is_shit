161017bd02ad714e3bbac21f8cb0f43d4dc6a5d6
Vladislav <vsoilol1701@gmail.com>
2022-04-02 19:26:14 +0300

    Create FileSystemTreeTests

---
+++ homework5/homework5_1/src/test/java/FileSystem/FileSystemTreeTests.java
@@ -0,0 +1,84 @@
+package FileSystem;
+
+import FileInfo.File;
+import FileInfo.FileComponent;
+import FileInfo.Folder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileSystemTreeTests {
+    private FileComponent folder1;
+    private final FileSystemTree fileSystemTree = new FileSystemTree();
+
+    @BeforeEach
+    public void setUp() {
+        folder1 = new Folder("folder1");
+    }
+
+    @Test
+    public void testPrintDirectoryTreeWhenFolderIsEmpty() {
+        String actual = fileSystemTree.printDirectoryTree(folder1);
+
+        assertEquals("+--folder1/\n", actual);
+    }
+
+    private static final String EXPECTED_ONE_FILE_IN_FOLDER = "" + "+--folder1/\n"
+            + "|  +--a.txt\n";
+
+    @Test
+    public void testPrintDirectoryTreeWhenFolderContainsOneFile() {
+        FileComponent file = new File("a", "txt");
+        folder1.addFileComponent(file);
+
+        String actual = fileSystemTree.printDirectoryTree(folder1);
+
+        assertEquals(EXPECTED_ONE_FILE_IN_FOLDER, actual);
+    }
+
+    private static final String EXPECTED_COMPLEX = "+--folder1/\n"
+            + "|  +--a.txt\n" + "|  +--folder2/\n" + "|  |  +--b1.txt\n"
+            + "|  |  +--b2.txt\n" + "|  |  +--b3.txt\n" + "|  |  +--folder3/\n"
+            + "|  +--folder4/\n";
+
+    @Test
+    public void testPrintDirectoryTreeComplex() {
+        addSeveralFoldersWithFiles();
+
+        String actual = fileSystemTree.printDirectoryTree(folder1);
+
+        assertEquals(EXPECTED_COMPLEX, actual);
+    }
+
+    @Test
+    public void testPrintDirectoryTreeWhenFolderIsFile() {
+        FileComponent file = new File("file1", "txt");
+
+        assertThrows(IllegalArgumentException.class, () -> fileSystemTree.printDirectoryTree(file));
+    }
+
+    private void addSeveralFoldersWithFiles() {
+        FileComponent aFile = new File("a", "txt");
+        folder1.addFileComponent(aFile);
+
+        FileComponent folder2 = new Folder("folder2");
+
+        FileComponent b1File = new File("b1", "txt");
+        FileComponent b2File = new File("b2", "txt");
+
+        folder2.addFileComponent(b1File);
+        folder2.addFileComponent(b2File);
+
+        folder1.addFileComponent(folder2);
+
+        FileComponent b3File = new File("b3", "txt");
+        folder2.addFileComponent(b3File);
+
+        FileComponent folder3 = new Folder("folder3");
+        folder2.addFileComponent(folder3);
+
+        FileComponent folder4 = new Folder("folder4");
+        folder1.addFileComponent(folder4);
+    }
+}
