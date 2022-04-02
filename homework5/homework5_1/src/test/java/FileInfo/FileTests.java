03e4c6f9d09865e4f644c678259a4ddc3d32a9ac
Vladislav <vsoilol1701@gmail.com>
2022-04-02 19:34:53 +0300

    Create FileTests

---
+++ homework5/homework5_1/src/test/java/FileInfo/FileTests.java
@@ -0,0 +1,41 @@
+package FileInfo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FileTests {
+    private File file;
+
+    private final String name = "file";
+    private final String extension = "txt";
+
+    @BeforeEach
+    public void init() {
+        file = new File(name, extension);
+    }
+
+    @Test
+    public void testGetNameShouldWorkAsExpected() {
+        String expected = name + "." + extension;
+
+        String actual = file.getName();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddFileComponentShouldThrowUnsupportedOperationException() {
+        FileComponent folder = new Folder("newFolder");
+        assertThrows(UnsupportedOperationException.class, () -> file.addFileComponent(folder));
+    }
+
+    @Test
+    public void testGetFileComponentByNameShouldThrowUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> file.getFileComponentByName("newFolder"));
+    }
+}
