2e4b209bf27e1b90d2fdd63b0825d2e95cc7c1b4
Vladislav <vsoilol1701@gmail.com>
2022-05-09 11:53:26 +0300

    Create task 7

--- homework7/src/main/java/ValidationSystem.java
+++ homework7/src/main/java/ValidationSystem.java
@@ -1,18 +1,10 @@
-
-import java.util.HashMap;
-import java.util.Map;
-
 public final class ValidationSystem {
-    private static final Map<Class, Validator> validators = new HashMap<>() {{
-        put(Integer.class, new IntegerValidator());
-        put(String.class, new StringValidator());
-    }};
-
     public static <T> boolean validate(T value) {
-        if(!validators.containsKey(value.getClass())){
-            return false;
+        if (Integer.class.equals(value.getClass())) {
+            return (new IntegerValidator()).isValid((Integer) value);
+        } else if (String.class.equals(value.getClass())) {
+            return (new StringValidator()).isValid((String) value);
         }
-
-        return validators.get(value.getClass()).isValid(value);
+        return false;
     }
 }
